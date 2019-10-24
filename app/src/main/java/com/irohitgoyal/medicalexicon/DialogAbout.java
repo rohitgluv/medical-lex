7653d251b04156bb7e82c35c4a130c3b124ed976
Rohit Goyal <irohitgoyal@gmail.com>
2019-10-24 08:54:22 +0530

    Update DialogAbout.java

--- app/src/main/java/com/irohitgoyal/medicalexicon/DialogAbout.java
+++ app/src/main/java/com/irohitgoyal/medicalexicon/DialogAbout.java
@@ -13,42 +13,49 @@ import android.widget.TextView;
 /**
  * Created by ROHIT on 3/5/2016.
  */
-public class DialogAbout extends DialogFragment
-        implements View.OnClickListener
+
+public class DialogAbout extends DialogFragment implements View.OnClickListener
 {
     public void onCancel(DialogInterface paramDialogInterface)
     {
-        super.onCancel(paramDialogInterface);
+       super.onCancel(paramDialogInterface);
     }
 
     public void onClick(View paramView)
     {
-        switch (paramView.getId()){
+       switch (paramView.getId()){
 
         case R.id.textOtherApp:
         {
             Intent intentLink = new Intent("android.intent.action.VIEW", Uri.parse("https://play.google.com/store/apps/developer?id=" + getString(R.string.devPlay)));
             startActivity(intentLink);
+            
             break;
         }
     }
+    
     dismiss();
 }
 
     public View onCreateView(LayoutInflater paramLayoutInflater, ViewGroup paramViewGroup, Bundle paramBundle)
     {
         getDialog().requestWindowFeature(1);
+        
         View localView = paramLayoutInflater.inflate(R.layout.dialog_about, null);
+        
         localView.findViewById(R.id.btnYes).setOnClickListener(this);
         //localView.findViewById(R.id.textViewFullHref).setOnClickListener(this);
+        
         localView.findViewById(R.id.textOtherApp).setOnClickListener(this);
+        
         ((TextView)localView.findViewById(R.id.textVers)).setText("1.1");
         return localView;
     }
 
+
     public void onDismiss(DialogInterface paramDialogInterface)
     {
-        super.onDismiss(paramDialogInterface);
+       super.onDismiss(paramDialogInterface);
     }
 }
 
